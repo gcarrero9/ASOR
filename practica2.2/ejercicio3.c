f56ab91a1db2ebe1a0e7d56d5a874fab1a903a9f
root <root@localhost.localdomain>
2020-12-11 21:36:01 +0100

    practica2.2

---
+++ practica2.2/ejercicio3.c
@@ -0,0 +1,4 @@
+//Ejercicio 3: Fijar los permisos rw-r--r-x de las 2 formas
+
+chmod 645 fichero
+chmod u+rw-x,g+r-wx,o+rx-w
---
+++ practica2.2/ejercicio4.c
@@ -0,0 +1,6 @@
+//Crear un directorio y quitar los permisos de ejecucion para usuario, grupo y otros.
+//Intentar cambiar al directorio.
+
+Mkdir directorio
+Chmod 666 directorio
+cd directorio (permiso denegado)
